27bbe960cc81ed54945ff18ad4bd0fcae16d7f53
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-06-06 21:20:30 -0500

    add error messages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from . import util
 
 
@@ -7,4 +8,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def handler404(request, *args, **argv):
+    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+# def handler500(request, *args, **argv):
+#     response = render_to_response('500.html', {}, context_instance=RequestContext(request))
+#     response.status_code = 500
+#     return response
+# this is the catch all error
 
